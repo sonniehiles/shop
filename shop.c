5866abf755fade1c421e4321228c48238b85a130
Sonnie Hiles <Sonnie@Bruce.local>
2015-01-26 21:09:41 +0100

    First commit

---
+++ shop.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+// look at table for pricing
+// look at read input readln
+// extra = map to array
+
+struct item 
+{
+   int price;
+   char *description;
+};
+
+struct item items[] = {
+	{ .price = 123, .description = "bread" },
+	{ .price = 123, .description = "milk" },
+	{ .price = 123, .description = "cheese" }
+	};
+
+int number_of_items() 
+{
+	return sizeof(items) / sizeof(struct item);
+}
+
+void display_items() 
+{
+	int num_items = number_of_items();
+
+   for (int i = 0; i < num_items; i = i + 1)
+   {
+   	printf("Item %d: %s\n", i, items[i].description);
+   }
+}
+
+int main()
+{
+	display_items();
+}
